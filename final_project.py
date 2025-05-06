175a688d1a061fd757f316f412ca84db49b09409
christi <cgjp1411@gmail.com>
2025-05-05 18:04:05 -0600

    Create final_project.py

---
+++ final_project.py
@@ -0,0 +1,35 @@
+"""
+    Name: final_project.py
+    Author: Christi P.
+    Created: 5/5/25
+    Purpose: my final individual OOP project of the semester
+"""
+
+# vehicle = spaceship
+
+class Spaceship():
+    # ------ MAIN ----- #
+    def main_spaceship(self):
+        self.spaceship = spaceship
+        # ask for captain's name
+        # ask for what they want to name their ship
+        # ask for maximum capacity
+        # ask for maximum speed
+        pass
+
+    def ascent(self):
+        # take off from Earth
+        pass
+
+    def accelerate(self):
+        # acceleration method
+        # if input = a then accelerate
+        pass
+
+    def decelerate(self):
+        # deceleration method
+        # if input = d then decelerate
+        pass
+
+
+spaceship = Spaceship()
